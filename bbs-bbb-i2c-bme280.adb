0c9d9f47a0efa8e17514894e3088ad43f6a54fe6
Brent Seidel <brentseidel@mac.com>
2016-09-27 22:48:19 +0000

    Some more updates to the BME280 driver.  It still doesn't compile yet, but it's getting closer.  If I manage to get more than a few minutes here and there to work on it, I should be able to get it working.

--- bbs-bbb-i2c-bme280.adb
+++ bbs-bbb-i2c-bme280.adb
@@ -6,210 +6,6 @@ package body BBS.BBB.i2c.BME280 is
    function "**"(Left, Right : float) return float
                  renames float_functions."**";
    --
-   -- Given local pressure and altimeter setting, determine the pressure
-   -- altitude.  Given local pressure and altitude, determine the altimeter
-   -- setting.
-   --
-   function pressure_altitude(pressure : BBS.units.press_p; altm : BBS.units.press_p)
-                              return BBS.units.len_m is
-   begin
-      return BBS.units.len_m(44330.0 * (1.0 - (float(pressure)/float(altm))**float(1.0/5.255)));
-   end;
-   --
-   function altimeter(pressure : BBS.units.press_p; altitude : BBS.units.len_m) return
-     BBS.units.press_p is
-   begin
-      return BBS.units.press_p(float(pressure)/(1.0 - (float(altitude)/44330.0)**float(5.255)));
-   end;
-   --
-   -- Procedures to work with the BME280 pressure and temperature sensor
-   --
-   --
-   -- This procedure reads the calibration data from the sensor and uses it to
-   -- generate a number of calibration constants.
-   --
-   procedure configure(error : out integer) is
-   begin
-      T1 := readm2(addr, dig_T1, error);
-      T2 := uint16_to_int16(readm2(addr, dig_T2, error));
-      T3 := uint16_to_int16(readm2(addr, dig_T3, error));
-      P1 := readm2(addr, dig_P1, error);
-      P2 := uint16_to_int16(readm2(addr, dig_P2, error));
-      P3 := uint16_to_int16(readm2(addr, dig_P3, error));
-      P4 := uint16_to_int16(readm2(addr, dig_P4, error));
-      P5 := uint16_to_int16(readm2(addr, dig_P5, error));
-      P6 := uint16_to_int16(readm2(addr, dig_P6, error));
-      P7 := uint16_to_int16(readm2(addr, dig_P7, error));
-      P8 := uint16_to_int16(readm2(addr, dig_P8, error));
-      P9 := uint16_to_int16(readm2(addr, dig_P9, error));
-      H1 := read(addr, dig_H1, error);
-      H2 := uint16_to_int16(readm2(addr, dig_H2, error));
-      H3 := read(addr, dig_H3, error);
-      H4 := uint16_to_int16(readm2(addr, dig_H4, error));
-      H5 := uint16_to_int16(readm2(addr, dig_H5, error));
-      H6 := read(addr, dig_H6, error);
-   end;
-   --
---   procedure start_conversion(kind : uint8; error : out integer) is
---   begin
---      BBS.BBB.i2c.write(addr, ctrl, kind, error);
---      last_cvt := kind;
---   end;
-   --
---   function data_ready(error : out integer) return boolean is
---      byte : uint8;
---      err : integer;
---   begin
---      byte := BBS.BBB.i2c.read(addr, ctrl, err);
---      error := err;
---      if ((byte and start_cvt) /= start_cvt) and (err = 0) then
---         return true;
---      else
---         return false;
---      end if;
---   end;
-   --
-   -- The calculations to get calibrated temperature and pressure are based on
-   -- the algorithm in the datasheet.  It does not explain the meaning of the
-   -- various constants or steps in the process.  It is also not unlikely that
-   -- some errors may have crept in in the process of translating it into Ada.
-   --
-   -- I think that some of the arithmatic depends on the details of integer math
-   -- dropping bits on underflow or overflow.
-   --
-   function get_temp(error : out integer) return integer is
-      msb_value : uint8;
-      lsb_value : uint8;
-      temp : int16;
-   begin
-      if (last_cvt /= cvt_temp) then
-         Ada.Text_IO.Put_Line("Last conversion request was not for temperature");
-         raise Program_Error;
-      end if;
-      msb_value := BBS.BBB.i2c.read(addr, msb, error);
-      lsb_value := BBS.BBB.i2c.read(addr, lsb, error);
-      temp := uint16_to_int16(uint16(msb_value) * 256 + uint16(lsb_value));
-      x1 := ((integer(temp) - integer(ac6)) * integer(ac5)) / 32768;
-      x2 := integer(mc) * 2048 / (x1 + integer(md));
-      b5 := x1 + x2;
-      return (b5 + 8)/16;
-   end;
-   --
-   function get_temp(error : out integer) return float is
-      int_temp : integer := get_temp(error);
-   begin
-      return float(int_temp) / 10.0;
-   end;
-   --
-   function get_temp(error : out integer) return BBS.units.temp_c is
-      int_temp : integer := get_temp(error);
-   begin
-      return BBS.units.temp_c(float(int_temp) / 10.0);
-   end;
-   --
-   function get_temp(error : out integer) return BBS.units.temp_f is
-      int_temp : integer := get_temp(error);
-   begin
-      return BBS.units.to_Farenheit(BBS.units.temp_c(float(int_temp) / 10.0));
-   end;
-   --
-   function get_temp(error : out integer) return BBS.units.temp_k is
-      int_temp : integer := get_temp(error);
-   begin
-      return BBS.units.to_Kelvin(BBS.units.temp_c(float(int_temp) / 10.0));
-   end;
-   --
-   function get_press(error : out integer) return integer is
-      msb_value : uint8;
-      lsb_value : uint8;
-      xlsb_value : uint8;
-      oss : uint8;
-      oss_2 : integer;
-      press : integer;
-      b6 : integer;
-      x1a : integer;
-      x2a : integer;
-      x3 : integer;
-      b3 : integer;
-      b4 : uint32;
-      b7 : uint32;
-   begin
-      if (last_cvt /= cvt_press0) and (last_cvt /= cvt_press1) and
-        (last_cvt /= cvt_press2) and (last_cvt /= cvt_press3) then
-         Ada.Text_IO.Put_Line("Last conversion request was not for pressure");
-         raise Program_Error;
-      end if;
-      msb_value := BBS.BBB.i2c.read(addr, msb, error);
-      lsb_value := BBS.BBB.i2c.read(addr, lsb, error);
-      xlsb_value := BBS.BBB.i2c.read(addr, xlsb, error);
-      press := uint32_to_int(uint32(msb_value) * 65536 + uint32(lsb_value) * 256 + uint32(xlsb_value));
-      oss := (last_cvt / 64) and 3;
-      case oss is
-      when 0 =>
-         press := press / 2 ** 8;
-         oss_2 := 1;
-      when 1 =>
-         press := press / 2 ** 7;
-         oss_2 := 2;
-      when 2 =>
-         press := press / 2 ** 6;
-         oss_2 := 4;
-      when 3 =>
-         press := press / 2 ** 5;
-         oss_2 := 8;
-      when others =>
-         Ada.Text_IO.Put_Line("OSS value out of range " & integer'Image(integer(oss)));
-         raise Program_Error;
-      end case;
-      b6 := b5 - 4000;
-      x1a := (integer(b2) * (b6*b6/2**12))/2**11;
-      x2a := integer(ac2)*b6/2**11;
-      x3 := x1a + x2a;
-      b3 := (((integer(ac1)*4 + x3)*oss_2) + 2)/4;
-      x1a := integer(ac3)*b6/2**13;
-      x2a := (integer(b1) * (b6*b6/2**12))/2**16;
-      x3 := (x1a + x2a + 2)/2**2;
-      b4 := uint32(ac4) * uint32(x3 + 32768)/2**15;
-      b7 := (int_to_uint32(press) - uint32(b3))*(50000/uint32(oss_2));
-      if (b7 < 16#80000000#) then
-         press := integer((b7*2)/b4);
-      else
-         press := integer((b7/b4)*2);
-      end if;
-      x1a := (press/2**8)*(press/2**8);
-      x1a := (x1a*3038)/2**16;
-      x2a := (-7357*press)/2**16;
-      press := press + (x1a + x2a + 3791)/2**4;
-      return press;
-   end;
-   --
-   function get_press(error : out integer) return BBS.units.press_p is
-      int_press : integer;
-   begin
-      int_press := get_press(error);
-      return BBS.units.press_p(int_press);
-   end;
-   --
-   function get_press(error : out integer) return BBS.units.press_mb is
-      int_press : integer;
-   begin
-      int_press := get_press(error);
-      return BBS.units.to_milliBar(BBS.units.press_p(int_press));
-   end;
-   --
-   function get_press(error : out integer) return BBS.units.press_atm is
-      int_press : integer;
-   begin
-      int_press := get_press(error);
-      return BBS.units.to_Atmosphere(BBS.units.press_p(int_press));
-   end;
-   --
-   function get_press(error : out integer) return BBS.units.press_inHg is
-      int_press : integer;
-   begin
-      int_press := get_press(error);
-      return BBS.units.to_inHg(BBS.units.press_p(int_press));
-   end;
    --
    -- Object oriented interface
    --
@@ -245,13 +41,31 @@ package body BBS.BBB.i2c.BME280 is
       self.H4 := uint16_to_int16(self.port.readm2(self.address, dig_H4, error));
       self.H5 := uint16_to_int16(self.port.readm2(self.address, dig_H5, error));
       self.H6 := self.port.read(self.address, dig_H6, error);
+      --
+      -- Now set the mode.  Use forced mode to keep the interface similar to
+      -- the BMP180.
+      --
+      -- First put into sleep more so configuration can be set.  Oversampling
+      -- is set to 1 for each parameter.
+      --
+      self.port.write(self.address, ctrl_meas, mode_sleep, error);
+      --
+      -- Set humidity oversampling
+      --
+      self.port.write(self.address, ctrl_hum, hum_over_1, error);
+      --
+      -- Temperature, pressure, and mode are in the same register so set them
+      -- all at once.
+      --
+      self.port.write(self.address, ctrl_meas, temp_over_1 + press_over_1 +
+                        mode_force, error);
    end;
    --
    procedure start_conversion(self : not null access BME280_record'class;
-                              kind : uint8; error : out integer) is
+                              error : out integer) is
    begin
-      self.port.write(self.address, ctrl, kind, error);
-      self.last_cvt := kind;
+      self.port.write(self.address, ctrl_meas, temp_over_1 + press_over_1 +
+                        mode_force, error);
    end;
    --
    function data_ready(self : not null access BME280_record'class;
@@ -259,13 +73,30 @@ package body BBS.BBB.i2c.BME280 is
       byte : uint8;
       err : integer;
    begin
-      byte := self.port.read(self.address, ctrl, err);
+      byte := self.port.read(self.address, status, err);
       error := err;
-      if ((byte and start_cvt) /= start_cvt) and (err = 0) then
+      if ((byte and stat_measuring) /= stat_measuring) and (err = 0) then
          return true;
       else
          return false;
       end if;
+   end;
+   --
+   -- Read 3 bytes for pressure and temperature and 2 bytes for humidity. 8 bytes
+   -- total
+   --
+   procedure read_data(self : not null access BME280_record'class; error : out integer) is
+   begin
+      self.port.read(addr, data_start, buff'access, 8, error);
+      self.press_msb  := buff(0);
+      self.press_lsb  := buff(1);
+      self.press_xlsb := buff(2);
+      self.temp_msb  := buff(3);
+      self.temp_lsb  := buff(4);
+      self.temp_xlsb := buff(5);
+      self.hum_msb := buff(6);
+      self.hum_lsb := buff(7);
+
    end;
    --
    function get_temp(self : not null access BME280_record'class;
@@ -274,10 +105,6 @@ package body BBS.BBB.i2c.BME280 is
       lsb_value : uint8;
       temp : int16;
    begin
-      if (self.last_cvt /= cvt_temp) then
-         Ada.Text_IO.Put_Line("Last conversion request was not for temperature");
-         raise Program_Error;
-      end if;
       msb_value := self.port.read(self.address, msb, error);
       lsb_value := self.port.read(self.address, lsb, error);
       temp := uint16_to_int16(uint16(msb_value) * 256 + uint16(lsb_value));
@@ -331,11 +158,6 @@ package body BBS.BBB.i2c.BME280 is
       b4a : uint32;
       b7a : uint32;
    begin
-      if (self.last_cvt /= cvt_press0) and (self.last_cvt /= cvt_press1) and
-        (self.last_cvt /= cvt_press2) and (self.last_cvt /= cvt_press3) then
-         Ada.Text_IO.Put_Line("Last conversion request was not for pressure");
-         raise Program_Error;
-      end if;
       msb_value := self.port.read(self.address, msb, error);
       lsb_value := self.port.read(self.address, lsb, error);
       xlsb_value := self.port.read(self.address, xlsb, error);
--- bbs-bbb-i2c-bme280.ads
+++ bbs-bbb-i2c-bme280.ads
@@ -19,9 +19,10 @@ package BBS.BBB.i2c.BME280 is
    --
    -- Addresses for the BME280 pressure and temperature sensor
    --
-   addr : constant addr7 := 16#77#;
-   ut : constant uint8 := 16#f7#; -- uncomponsated temperature (msb, lsb, xlsb)
-   up : constant uint8 := 16#fa#; -- uncomponsated pressure (msb, lsb, xlsb)
+   addr : constant addr7 := 16#77#; -- Device address on I2C bus
+   data_start : constant uint8 := 16#f7#;
+   up : constant uint8 := 16#f7#; -- uncomponsated pressure (msb, lsb, xlsb)
+   ut : constant uint8 := 16#fa#; -- uncomponsated temperature (msb, lsb, xlsb)
    uh : constant uint8 := 16#fd#; -- uncomponsated humidity (msb, lsb)
    ctrl_hum : constant uint8 := 16#f2#;
    status : constant uint8 := 16#f3#;
@@ -48,26 +49,61 @@ package BBS.BBB.i2c.BME280 is
    dig_H5 : constant uint8 := 16#e5#; -- int16
    dig_H6 : constant uint8 := 16#e7#; -- uint8
    --
-   -- Command constants
+   -- Mode constants
    --
-   start_cvt : constant uint8 := 16#20#;
+   mode_sleep  : constant uint8 := 2#000_000_00#;
+   mode_force  : constant uint8 := 2#000_000_10#; -- 2#01# also works
+   mode_normal : constant uint8 := 2#000_000_11#;
+   --
+   -- Oversampling constants
+   -- Humidity
+   --
+   hum_over_0  : constant uint8 := 2#000#; -- datasheet says skipped
+   hum_over_1  : constant uint8 := 2#001#;
+   hum_over_2  : constant uint8 := 2#010#;
+   hum_over_4  : constant uint8 := 2#011#;
+   hum_over_8  : constant uint8 := 2#100#;
+   hum_over_16 : constant uint8 := 2#101#; -- apparently the other values work as well
+   --
+   -- Pressure
+   --
+   press_over_0  : constant uint8 := 2#000_000_00#;
+   press_over_1  : constant uint8 := 2#001_000_00#;
+   press_over_2  : constant uint8 := 2#010_000_00#;
+   press_over_4  : constant uint8 := 2#011_000_00#;
+   press_over_8  : constant uint8 := 2#100_000_00#;
+   press_over_16 : constant uint8 := 2#101_000_00#;
+   --
+   -- Temperature
+   --
+   temp_over_0  : constant uint8 := 2#000_000_00#;
+   temp_over_1  : constant uint8 := 2#000_001_00#;
+   temp_over_2  : constant uint8 := 2#000_010_00#;
+   temp_over_4  : constant uint8 := 2#000_011_00#;
+   temp_over_8  : constant uint8 := 2#000_100_00#;
+   temp_over_16 : constant uint8 := 2#000_101_00#;
+   --
+   -- Status bits
+   --
+   stat_measuring : constant uint8 := 2#0000_1000#;
+   stat_im_update : constant uint8 := 2#0000_0001#;
    --
    -- Conversion types
    --
-   cvt_temp : constant uint8 := 16#2e#;
-   cvt_press0 : constant uint8 := 16#34#;
-   cvt_press1 : constant uint8 := 16#74#;
-   cvt_press2 : constant uint8 := 16#b4#;
-   cvt_press3 : constant uint8 := 16#f4#;
+--   cvt_temp : constant uint8 := 16#2e#;
+--   cvt_press0 : constant uint8 := 16#34#;
+--   cvt_press1 : constant uint8 := 16#74#;
+--   cvt_press2 : constant uint8 := 16#b4#;
+--   cvt_press3 : constant uint8 := 16#f4#;
    --
    -- Given local pressure and altimeter setting, determine the pressure
    -- altitude.  Given local pressure and altitude, determine the altimeter
    -- setting.
    --
-   function pressure_altitude(pressure : BBS.units.press_p;
-                              altm : BBS.units.press_p) return BBS.units.len_m;
-   function altimeter(pressure : BBS.units.press_p;
-                      altitude : BBS.units.len_m) return BBS.units.press_p;
+--   function pressure_altitude(pressure : BBS.units.press_p;
+--                              altm : BBS.units.press_p) return BBS.units.len_m;
+--   function altimeter(pressure : BBS.units.press_p;
+--                      altitude : BBS.units.len_m) return BBS.units.press_p;
    --
    -- The configure procedure needs to be called first to initialize the
    -- calibration constants from the device.
@@ -82,7 +118,7 @@ package BBS.BBB.i2c.BME280 is
    -- get_temp or get_press is called, the type is checked to ensure that the
    -- proper reading was started.
    --
-   procedure configure(error : out integer);
+--   procedure configure(error : out integer);
    --
    -- Starts the BME280 converting data.  The conversion types are listed above.
    -- Note that the conversion type must match the get value function.  An
@@ -98,30 +134,30 @@ package BBS.BBB.i2c.BME280 is
    -- Return a calibrated temperature value.  Temperature is returned in units
    -- of degrees Celsius.
    --
-   function get_temp(error : out integer) return float;
+--   function get_temp(error : out integer) return float;
    --
    -- Return calibrated temperature in units of 0.1 degrees Celsius.
    --
-   function get_temp(error : out integer) return integer;
+--   function get_temp(error : out integer) return integer;
    --
    -- Return temperature in various units.
    --
-   function get_temp(error : out integer) return BBS.units.temp_c;
-   function get_temp(error : out integer) return BBS.units.temp_f;
-   function get_temp(error : out integer) return BBS.units.temp_k;
+--   function get_temp(error : out integer) return BBS.units.temp_c;
+--   function get_temp(error : out integer) return BBS.units.temp_f;
+--   function get_temp(error : out integer) return BBS.units.temp_k;
    --
    -- Return a calibrated pressure value.  Note that a temperature reading must
    -- be made before calibrated pressure can be successfully computed.  Pressure
    -- is returned in units of Pascals.
    --
-   function get_press(error : out integer) return integer;
+--   function get_press(error : out integer) return integer;
    --
    -- Return pressure in various units.
    --
-   function get_press(error : out integer) return BBS.units.press_p;
-   function get_press(error : out integer) return BBS.units.press_mb;
-   function get_press(error : out integer) return BBS.units.press_atm;
-   function get_press(error : out integer) return BBS.units.press_inHg;
+--   function get_press(error : out integer) return BBS.units.press_p;
+--   function get_press(error : out integer) return BBS.units.press_mb;
+--   function get_press(error : out integer) return BBS.units.press_atm;
+--   function get_press(error : out integer) return BBS.units.press_inHg;
    --
    -- Stuff for object oriented interface.  These basically emulate the function
    -- of the conventional routines above.
@@ -133,8 +169,9 @@ package BBS.BBB.i2c.BME280 is
    procedure configure(self : not null access BME280_record'class; port : i2c_interface;
                        addr : addr7; error : out integer);
    --
-   procedure start_conversion(self : not null access BME280_record'class; kind : uint8; error : out integer);
+   procedure start_conversion(self : not null access BME280_record'class; error : out integer);
    function data_ready(self : not null access BME280_record'class; error : out integer) return boolean;
+   procedure read_data(self : not null access BME280_record'class; error : out integer);
    function get_temp(self : not null access BME280_record'class; error : out integer) return float;
    function get_temp(self : not null access BME280_record'class; error : out integer) return integer;
    function get_temp(self : not null access BME280_record'class; error : out integer) return BBS.units.temp_c;
@@ -176,7 +213,6 @@ private
    x2 : integer;
    b5 : integer;
    --
-   last_cvt : uint8 := 0;
    --
    -- Some unchecked conversions are needed in pressure conversion.
    --
@@ -205,10 +241,23 @@ private
       H4 : int16 := 0;
       H5 : int16 := 0;
       H6 : uint8 := 0;
+      --
+      --  Values from temperature conversion (from BMP180 may now be different)
+      --
       x1 : integer;
       x2 : integer;
       b5 : integer;
-      last_cvt : uint8 := 0;
+      --
+      -- Data read from device
+      --
+      press_msb : uint8;
+      press_lsb : uint8;
+      press_xlsb : uint8;
+      temp_msb : uint8;
+      temp_lsb : uint8;
+      temp_xlsb : uint8;
+      hum_msb : uint8;
+      hum_lsb : uint8;
    end record;
 
 end;
