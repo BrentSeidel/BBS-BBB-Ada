68b965eef5babe3733f0fd8d2e35e1477ff11d77
Brent Seidel <brentseidel@mac.com>
2016-11-15 16:40:22 -0700

    Add basic operations for the MCP23008 8 bit digital I/O port on an I2C bus. This is used by the AdaFruit I2C LCD backpack and is intended to be used for that purpose.  Should you need other features of the MCP23008, it should be easy to add them.

---
+++ bbs-bbb-i2c-mcp23008.adb
@@ -0,0 +1,37 @@
+package body BBS.BBB.i2c.MCP23008 is
+   --
+   --
+   -- Object oriented interface
+   --
+   function i2c_new return MCB23008_ptr is
+   begin
+      return new MCB23008_record;
+   end;
+   --
+   procedure configure(self : not null access MCB23008_record'class; port : i2c_interface;
+                       addr : addr7; error : out integer) is
+      pragma Unreferenced(error);
+   begin
+      self.port := port;
+      self.address := addr;
+   end;
+   --
+   procedure set_dir(self : not null access MCB23008_record'class; dir : uint8;
+                     error : out integer) is
+   begin
+      self.port.write(self.address, IODIR, dir, error);
+   end;
+   --
+   procedure set_data(self : not null access MCB23008_record'class; data : uint8;
+                      error : out integer) is
+   begin
+      self.port.write(self.address, GPIO, data, error);
+   end;
+   --
+   function read_data(self : not null access MCB23008_record'class; error : out integer)
+                      return uint8 is
+   begin
+      return self.port.read(self.address, GPIO, error);
+   end;
+
+end;
