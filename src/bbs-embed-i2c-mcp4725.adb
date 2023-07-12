08c6e6df7f5a7cdae319e847876b2967642e6158
Brent Seidel <brentseidel@mac.com>
2023-07-12 11:07:25 -0700

    Updates for MCP4725.  Still a work in progress.

--- src/bbs-embed-i2c-mcp4725.adb
+++ src/bbs-embed-i2c-mcp4725.adb
@@ -1,6 +1,7 @@
 --
 --  Package body for driving the MCP4725 DAC.
 --
+with BBS.embed.log;
 package body BBS.embed.i2c.MCP4725 is
    --
    procedure configure(self : in out MCP4725_record; port : i2c_interface;
@@ -32,10 +33,39 @@ package body BBS.embed.i2c.MCP4725 is
    procedure set(self : in out MCP4725_record; value : uint12; err : out err_code) is
    begin
      --
-     --  Fast write in normal mode.
+     --  Fast write in normal mode.  This just sends two bytes to the
+     --  DAC.  The write register command is used for this with the
+     --  register address being the high byte and the data being the
+     --  low byte.
      --
-     self.hw.write(self.address, BBS.embed.uint8((value/16#100#) and 16#0F#),
-      BBS.embed.uint8(value and 16#FF#), err);
+     self.hw.write(self.address, uint8((value/16#100#) and 16#0F#),
+                   uint8(value and 16#FF#), err);
    end;
    --
+   --
+   --  General set command.  Use the defined constants for the command
+   --  and mode.  Other values may cause unexpected behavior.
+   --
+   procedure set(self : in out MCP4725_record; cmd : CMD_type; mode : Mode_type;
+                 value : uint12; err : out err_code) is
+     msb : uint8 := uint8(cmd)*16#20# + uint8(mode)*16#02#;
+   begin
+     if cmd = Fast_Write then
+       --
+       --  Fast write command.  This just sends two bytes to the  DAC
+       --  The write register command is used for this with the register
+       --  address being the high byte and the data being the low byte.
+       --
+       self.hw.write(self.address, uint8(mode)*16#10# + uint8((value/16#100#) and 16#0F#),
+                     uint8(value and 16#FF#), err);
+     else
+       --
+       --  Other commands.  This sends three bytes to the DAC.  The writem1
+       --  command is used for this.  The register address is the first
+       --  byte and the value is the two following bytes.
+       --
+--       BBS.embed.log.debug.Put_Line("Writing DAC value " & value'Image & ", address " & msb'Image);
+       self.hw.writem1(self.address, msb, uint16(value)*16#10#, err);
+     end if;
+   end;
 end;
--- src/bbs-embed-i2c-mcp4725.ads
+++ src/bbs-embed-i2c-mcp4725.ads
@@ -18,12 +18,25 @@ package BBS.embed.i2c.MCP4725 is
 --  6 - <Reserved>
 --  7 - <Reserved>
 --
+--  Note that the Fast Write command only requires two bytes to be sent
+--  to the DAC.  The other commands require three bytes.
+--
+   type CMD_type is new Integer range 0 .. 7;
+   Fast_Write   : constant CMD_type := 0;
+   Write_CMD    : constant CMD_type := 2;
+   Write_EEPROM : constant CMD_type := 3;
+-- 
 --  Power-Down select
 --  0 - Normal Mode
 --  1 - 1k resistance to ground
 --  2 - 100k resistance to ground
 --  3 - 500k resistance to ground
 --
+   type Mode_type is new Integer range 0 .. 3;
+   PD_Normal : constant Mode_type := 0;
+   PD_1k     : constant Mode_type := 1;
+   PD_100k   : constant Mode_type := 2;
+   PD_500k   : constant Mode_type := 3;
    --
    -- Define object.
    --
@@ -33,9 +46,16 @@ package BBS.embed.i2c.MCP4725 is
    procedure configure(self : in out MCP4725_record; port : i2c_interface;
                        addr : addr7; error : out err_code);
    --
-   --  The DAC is only single channel, so this just sets the value.
+   --  The DAC is only single channel, so this just sets the value using
+   --  fast write and PD nprmal mode.
    --
    procedure set(self : in out MCP4725_record; value : uint12; err : out err_code);
+   --
+   --  General set command.  Use the defined constants for the command
+   --  and mode.  Other values may cause unexpected behavior.
+   --
+   procedure set(self : in out MCP4725_record; cmd : CMD_type; mode : Mode_type;
+                 value : uint12; err : out err_code);
    --
 private
    --
